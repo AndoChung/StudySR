bc0eb80d30d4b09bd87b3ba8e07517311e552ac9
Andy Chung <68882499+AndoChung@users.noreply.github.com>
2024-06-10 23:26:28 -0700

    Initialized Sequelize to connect to postgreSQL

--- server/server.js
+++ server/server.js
@@ -1,11 +1,19 @@
 import express from "express";
+import db from "./config/database.js";
 
 const app = express();
 
-app.get("/", (req,res) => {
-    res.send("hello this is my server")
-})
+try {
+  await db.authenticate();
+  console.log("Connection has been established successfully.");
+} catch (error) {
+  console.error("Unable to connect to the database:", error);
+}
+
+app.get("/", (req, res) => {
+  res.send("hello this is my server");
+});
 
 app.listen(3001, () => {
-    console.log("server is up")
-})
+  console.log("server is up");
+});
