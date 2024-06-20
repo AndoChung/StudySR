e30a6ab71816a837d1618337f2bb5a95f51a9405
Andy Chung <68882499+AndoChung@users.noreply.github.com>
2024-06-19 19:26:59 -0700

    Initialized Vite and Tailwind for Client

--- server/config/database.js
+++ server/config/database.js
@@ -1,7 +1,5 @@
 import { Sequelize } from "sequelize";
 
-console.log(process.env)
-
 const db = new Sequelize("postgres", process.env.DB_USERNAME, process.env.DB_PASSWORD, {
   host: "aws-0-us-west-1.pooler.supabase.com",
   dialect: "postgres",
